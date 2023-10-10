106dc26d715d16464fbf64e8823ebe091e174e32
Dell-User <Dell-User@DESKTOP-GAPN8T9>
2023-10-10 09:13:18 +0200

    Add project files.

---
+++ 15.While loop/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _15.While_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 0;
+
+            Console.WriteLine("This is the while loop");
+            while (index <= 5) {
+
+                Console.WriteLine(index);
+                index++;
+
+                Console.WriteLine("This is a do while loop");
+
+                int index2 = 0;
+                do { Console.WriteLine(index2); } while (index2 <= 5);
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
